cd5a24191f530c4de0d13783e0be0ec0dc95f62b
InvariablyGib <aedansnyder@gmail.com>
2026-09-16 18:13:03 -0600

    Basic function prototypes laid out in array.h along with struct. Array.c handles the function definitons/implementations. still needs work in main.c and some tweaks probably due to errors.

--- array.h
+++ array.h
@@ -1,4 +1,17 @@
-#ifndef
+#ifndef ARRAY_H
+#define ARRAY_H
+
+struct _my_array
+{
+	int size;
+	double *data;
+};
+typedef struct _my_array Array;
+
+void output_array(Array *arr);
+void shift_array(Array *arr);
+Array *average_adjacent(Array *arr);
+//function prototypes. ouput and shift are void because they return nothing.everything is done with pointers. average_adjacent builds an entirely new array struct with updated values.
 
 
 #endif
